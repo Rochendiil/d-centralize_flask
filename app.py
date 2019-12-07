0a4fd5edfb53d48f25ad842636255a0061fb9ef7
Berg,Luuk L.L.M. van den <l.vandenberg@student.fontys.nl>
2019-12-07 17:36:29 +0100

    git ignore update

--- app.py
+++ app.py
@@ -6,46 +6,22 @@ from difflib import SequenceMatcher
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-
-def searchPostalCode(query):
-    result = [0,0]
-    sfPostal = shapefile.Reader('shapefiles/openpostcodevlakkenpc4.shp')
-    records = sfPostal.records()
-    for rec in records:
-        number = rec[0]
-        if number == query:
-            result[0] = number
-            result[1] = 1
-            return result[0]
-        if number.startswith(query):
-            if result[1] < SequenceMatcher(None, number, query).ratio():
-                result[0] = number
-                result[1] = SequenceMatcher(None, number, query).ratio()
-    return result[0]
-
-
-
-#searches given file for query result at index namefield
-def searchall(query, result, file, namefield):
-    sf = shapefile.Reader(file)
+#searches given file for query index of field
+def searchShapefileForQuery(query, result, shapefile, field):
+    sf = shapefile.Reader(shapefile)
     records = sf.records()
     #loops through all records
     for rec in records:
-        name = rec[namefield].lower()
-        #if name is exactly query set result[1](sequence percent) to 1
+        name = rec[field].lower()
+        #if name is exactly query set result[1](SequenceMatch ratio) to 1
         if name == query:
-            result[0] = rec[namefield]
+            result[0] = name
             result[1] = 1
             return result
         #if name starts with query check if result[1] is smaller then new Sequence if true result will be new record
         if name.startswith(query):
             if result[1] < SequenceMatcher(None, name, query).ratio():
-                result[0] = rec[namefield]
+                result[0] = name
                 result[1] = SequenceMatcher(None, name, query).ratio()
     return result
 
@@ -53,15 +29,16 @@ def searchall(query, result, file, namefield):
 @app.route('/requestQuery', methods=['POST'])
 def requestQuery():
     query = request.args.get('query')
+    # result[name, SequenceMatch ratio]
+    result = ['not found', 0]
     #check if query is a postcode e.g. [5432 AA]
     if re.findall('[0-9]', query):
-        return searchPostalCode(query)
+        result = searchShapefileForQuery(query, result, 'shapefiles/openpostcodevlakkenpc4.shp', 0)
     else:
         query = query.lower()
-        result = ['not found',0]
-        result = searchall(query, result, 'shapefiles/gis_osm_places_free_1.shp', 4)
-        result = searchall(query, result, 'shapefiles/gis_osm_roads_free_1.shp', 3)
-        return result[0]
+        result = searchShapefileForQuery(query, result, 'shapefiles/gis_osm_places_free_1.shp', 4)
+        result = searchShapefileForQuery(query, result, 'shapefiles/gis_osm_roads_free_1.shp', 3)
+    return result[0]
 
 if __name__ == '__main__':
     app.run()
